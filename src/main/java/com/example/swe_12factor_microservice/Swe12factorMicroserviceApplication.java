e02c13d53bc7ae5e6cae3980eb9e2e3f8081b358
franz <franz.schillinger@gmx.at>
2022-11-03 10:45:05 +0100

    Initial commit

---
+++ src/main/java/com/example/swe_12factor_microservice/Swe12factorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.swe_12factor_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe12factorMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swe12factorMicroserviceApplication.class, args);
+    }
+
+}
